bf701b0f051ca7357528d676726ee467c35fe677
ralaruri <ramzi.alaruri@gmail.com>
2024-01-19 23:04:12 -0500

    improving lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -50,6 +50,13 @@ func (l *Lexer) NextToken() token.Token {
     case 0:
       tok.Literal = ""
       tok.Type = token.EOF
+  default:
+    if isLetter(l.ch) {
+      tok.Literal = l.readIdentifer()
+      return tok  
+    } else {
+      tok = newToken(token.ILLEGAL, l.ch)
+    }
 
 
 }
@@ -57,6 +64,19 @@ func (l *Lexer) NextToken() token.Token {
   return tok
 }
 
+
 func newToken(tokenType token.TokenType, ch byte) token.Token {
   return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifer() string {
+  position := l.position
+  for isLetter(l.ch) {
+    l.readChar()
+  }
+  return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+  return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
